c85a4583bc797f860763720f53e825cc38660294
CatsEleven <hatijolove@gmail.com>
2024-04-07 22:37:00 +0900

    内容をクリアするところまで完了

--- 02_03_main.js
+++ 02_03_main.js
@@ -1,12 +1,29 @@
-let count = 0;
-
+count = 0;
 function buttonClicked(button) {
-    count++;
-    button.textContent = count;
+    if(!button.textContent){
+        count++;
+        button.textContent = count;
+        
+    }else{
+        button.textContent = null;
+        count--
+    }
 }
 
-document.getElementById('btn1').addEventListener('click', function() { buttonClicked(this); });
-document.getElementById('btn2').addEventListener('click', function() { buttonClicked(this); });
-document.getElementById('btn3').addEventListener('click', function() { buttonClicked(this); });
-document.getElementById('btn4').addEventListener('click', function() { buttonClicked(this); });
-document.getElementById('btn5').addEventListener('click', function() { buttonClicked(this); });
+// 全てのボタンに、関数を付与
+for (let i = 1; i <= 64; i++) {
+    let btnId = 'btn' + i;
+    document.getElementById(btnId).addEventListener('click', function() { buttonClicked(this); });
+  }
+
+document.getElementById("clear").addEventListener("click", clear)
+
+function clear(){
+    if(window.confirm("選択された場所をクリアします。よろしいですか？")){
+        for (let i = 1; i <= 64; i++) {
+            let btnId = 'btn' + i;
+            document.getElementById(btnId).textContent = '';
+          }
+}
+}
+
